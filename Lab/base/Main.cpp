795ec4d487d4997e71b1897cc207bc7c924869b8
Anastasya <akuzenkova1999@gmail.com>
2020-05-10 15:26:12 +0300

    Создала проект

---
+++ Lab/base/Main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a;
+	std::cout << "Hello world";
+	std::cin >> a;
+}
+
+//Именуем все переменный в стиле camelCase
+//Файлы .h и соответствующий ему .cpp называть одним именем (postfix.h, postfix.cpp)
